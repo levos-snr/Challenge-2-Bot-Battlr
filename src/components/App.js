86caaeafa2a37e8411f52b2943405a5fbdbf2974
Lewis Odero <lewisodero27@gmail.com>
2024-08-09 13:13:56 +0300

    test: passed props from app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import BotsPage from "./BotsPage";
 
 function App() {
+const [bots, setBots] = useState([]);
+
+useEffect(() => {
+  const fetchBots = async () => {
+    const response = await fetch("http://localhost:8002/bots");
+    if(!response.ok) {
+      throw new Error("Failed to fetch bots");
+    }
+    const data = await response.json();
+    setBots(data);
+  };
+  fetchBots();
+}, []);
+    
+ console.log("fetching bots", bots);
   return (
     <div className="App">
       <BotsPage />
